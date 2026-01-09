cd73262e1f138d6adb7877860d8da82064af4169
umi <zhaowenhai.zwh@alibaba-inc.com>
2026-01-09 15:03:25 +0800

    fix12

--- paimon-python/pypaimon/tests/torch_read_test.py
+++ paimon-python/pypaimon/tests/torch_read_test.py
@@ -323,7 +323,7 @@ class TorchReadTest(unittest.TestCase):
         table = self.catalog.get_table('default.test_multi_splits1')
         self._write_test_table(table)
 
-        num_workers = 2
+        num_workers = 0
         description = "aaa"
 
         read_builder = table.new_read_builder()
