b05bad89fbb46c44140d921e646c429a5d78fc2b
umi <zhaowenhai.zwh@alibaba-inc.com>
2026-01-09 13:46:36 +0800

    fix8_para

--- paimon-python/pypaimon/tests/torch_read_test.py
+++ paimon-python/pypaimon/tests/torch_read_test.py
@@ -63,7 +63,7 @@ class TorchReadTest(unittest.TestCase):
         dataloader = DataLoader(
             dataset,
             batch_size=2,
-            num_workers=0,
+            num_workers=2,
             shuffle=False
         )
 
